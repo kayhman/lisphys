c18b319a4c93e9b5e6275565071edce73c2f8e28
guillaume saupin <giullaume.saupin@gmail.com>
2013-05-21 16:12:27 +0200

    Added more tests.

--- quaternionad.lisp
+++ quaternionad.lisp
@@ -12,8 +12,23 @@
 (quat-z (from-axis e1a '(3.1416 0)))
 (quat-w (from-axis e1a '(3.1416 0)))
 
-(defun pos-quat (q1)
-    (.* (from-axis e1a q1) e1a))
+
+(vector3-x (.* (from-axis e1a '(3.1416 0)) e3a))
+(vector3-y (.* (from-axis e1a '(3.1416 0)) e3a))
+(vector3-z (.* (from-axis e1a '(3.1416 0)) e3a))
+
+(vector3-y e2a)
+
+(defun pos-quat (q1 q2)
+     (.* (from-axis e1a q1) (make-instance 'vector3ad :x '(1 0) :y '(.7 0) :z '(.6 0)))
+    )
+
+
+(defun pos-quat2 (q1 q2)
+    (.+ 
+     (.* (from-axis e1a q1) (make-instance 'vector3ad :x '(1 0) :y '(.6 0) :z '(.7 0)))
+     (.* e1a q2)
+    ))
 
 (type-of q1a)
 
@@ -28,6 +43,6 @@
 
 (.* (from-axis e1a '(1 2)) e1a)
 
-(vector3-x (d-first pos-quat ((q1 1.507))))
-(vector3-y (d-first pos-quat ((q1 1.507))))
-(vector3-z (d-first pos-quat ((q1 1.507))))
+(vector3-x (d-first pos-quat ((q1 0.) (q2 0.))))
+(vector3-y (d-first pos-quat ((q1 0.) (q2 0.))))
+(vector3-z (d-first pos-quat2 ((q1 0.) (q2 0.))))
