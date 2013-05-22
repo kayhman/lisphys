1018a10b726338ddbb038fad150df22d87c33cbd
guillaume saupin <giullaume.saupin@gmail.com>
2013-05-22 10:04:16 +0200

    Ease vector3ad declaration.
    *Added initialize-instance to allow (make-instance 'vector3ad :x 0 :y 1.0 : z '(1 2))

--- vector3ad.lisp
+++ vector3ad.lisp
@@ -4,6 +4,12 @@
   ((symb :initform 'vector3ad :allocation :class))
   )
 
+(defmethod initialize-instance :after ( (v vector3ad) &key x y z )
+	   (progn 
+	     (if (numberp x) (setf (vector3-x v ) `(,x 0.)))
+	     (if (numberp y) (setf (vector3-y v ) `(,y 0.)))
+	     (if (numberp z) (setf (vector3-z v ) `(,z 0.)))))
+
 (setq e1a (make-instance 'vector3ad :x '(1 0) :y '(0 0) :z '(0 0)) )
 (setq e2a (make-instance 'vector3ad :x '(0 0) :y '(1 0) :z '(0 0)) )
 (setq e3a (make-instance 'vector3ad :x '(0 0) :y '(0 0) :z '(1 0)) )
