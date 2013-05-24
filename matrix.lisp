058156b69737596376dd5e08ea6fff3942302884
guillaume saupin <giullaume.saupin@gmail.com>
2013-05-24 16:22:26 +0200

    Start matrix implementation.
    * Creation and element access added.

--- matrix.lisp
+++ matrix.lisp
@@ -1,3 +1,17 @@
 (defclass matrix (math)
-  ((val :accessor matrix-val :initarg :val :initform (vector 1 2 3)))
+  ((val :accessor matrix-val :initarg :val)
+   (nrows :accessor matrix-nrows :initarg :nrows :initform 0)
+   (ncols :accessor matrix-ncols :initarg :ncols :initform 0))
   )
+
+(defmethod initialize-instance :after ((m matrix) &key)
+  (setf (matrix-val m) 
+	(make-array (* (matrix-nrows m) (matrix-ncols m)))
+	 ))
+
+(defmethod melt ((m matrix) (i integer) (j integer))
+  (with-slots (nrows ncols val) m
+    (if (and (< i nrows) (< j ncols))
+	(elt val
+	     (+ j (* i (matrix-ncols m)))
+	    ))))
