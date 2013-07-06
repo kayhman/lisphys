e6a1f755eb2bb6d7dacc2e31cc67ee934de078ff
Guillaume <guillaume.saupin@gmail.com>
2013-07-06 15:32:00 +0200

    Add test with eigen.
    * To compare with lisphys.

---
+++ extra/testTwist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <Eigen/Lgsm>
+
+
+int main(int argc, char* argv [])
+{
+  Eigen::Vector3d pos(1, 0, 0);
+  Eigen::Displacementd H_1_0 = Eigen::Displacementd(pos);
+  Eigen::Displacementd H_2_1 = Eigen::Displacementd(pos);
+
+  Eigen::Vector3d axis(0, 0, 1);
+
+  Eigen::Twistd tw(Eigen::AngularVelocityd(axis), pos.cross(axis));
+
+  tw *=  M_PI / 2.0;
+ 
+  Eigen::Displacementd H_2_0 = tw.exp()  * H_1_0 * H_2_1;
+
+
+  std::cout << "twist : " << tw.transpose() << std::endl;
+  std::cout << "H_1_0 : " << H_1_0 << std::endl;
+  std::cout << "H_2_1 : " << H_2_1 << std::endl;
+  std::cout << "Brockett factor : " << tw.exp()  * H_1_0 << std::endl;
+  std::cout << "pos H_2_0 " << H_2_0.getTranslation().transpose() << std::endl;
+  
+  return 0;
+}
