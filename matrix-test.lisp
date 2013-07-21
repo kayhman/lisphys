9e9e84d424295256d8215c6260b873e083e514b3
Guillaume <guillaume.saupin@gmail.com>
2013-07-21 19:48:41 +0200

    Added test for matrix mupltiplication.

--- matrix-test.lisp
+++ matrix-test.lisp
@@ -3,13 +3,18 @@
 
 (defsuite MatrixSuite (LisPhysSuite))
 
-(deftest test-matrix (MatrixSuite)
+(deftest test-matrix-arithemtic (MatrixSuite)
   (let ((m1 #m((1 2) (3 4) (5 6)))
 	(m2 #m((1 2) (3 4) (5 6)))
 	(m3 #m((2 4) (6 8) (10 12)))
-	(m4 #m ((0 0) (0 0) (0 0))))
+	(m4 #m ((0 0) (0 0) (0 0)))
+	(m5 #m((1 2 3) (4 5 6)))
+	(m6 #m((1 2) (3 4) (5 6)))
+	(m7 #m((22 28) (49 64)))
+	)
     (assert-equal t (equalp  (matrix-val m3) (matrix-val (.+ m1 m2))))
     (assert-equal t (equalp  (matrix-val m4) (matrix-val (.- m1 m2))))
+    (assert-equal t (equalp  (matrix-val m7) (matrix-val (.* m5 m6))))
     ))
 
 (run-suite 'MatrixSuite)
