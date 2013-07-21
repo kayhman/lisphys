635bad28392e44242f8aab8dad50dc89366b9b3a
Guillaume <guillaume.saupin@gmail.com>
2013-07-21 10:14:44 +0200

    Added initial test for matrix.
    * Add/substract

--- matrix-test.lisp
+++ matrix-test.lisp
@@ -1,2 +1,15 @@
 (in-package #:lisphys)
-; todo
+
+
+(defsuite MatrixSuite (LisPhysSuite))
+
+(deftest test-matrix (MatrixSuite)
+  (let ((m1 #m((1 2) (3 4) (5 6)))
+	(m2 #m((1 2) (3 4) (5 6)))
+	(m3 #m((2 4) (6 8) (10 12)))
+	(m4 #m ((0 0) (0 0) (0 0))))
+    (assert-equal t (equalp  (matrix-val m3) (matrix-val (.+ m1 m2))))
+    (assert-equal t (equalp  (matrix-val m4) (matrix-val (.- m1 m2))))
+    ))
+
+(run-suite 'MatrixSuite)
