0304a7d75f4e20622891d8c062100dc3b4fd68e6
Guillaume <guillaume.saupin@gmail.com>
2013-07-22 23:42:51 +0200

    More display.

--- extra/testTwist.cpp
+++ extra/testTwist.cpp
@@ -12,17 +12,16 @@ int main(int argc, char* argv [])
 
   Eigen::Twistd tw(Eigen::AngularVelocityd(axis), pos.cross(axis));
 
-  tw *=  M_PI / 2.0;
+  //tw *=  2 * M_PI / 2.0;
 
   // See Murray Lie Sastry page 50. 
   Eigen::Displacementd H_2_0 = tw.exp()  * H_1_0 * H_2_1;
-
-
   std::cout << "twist : " << tw.transpose() << std::endl;
   std::cout << "H_1_0 : " << H_1_0 << std::endl;
   std::cout << "H_2_1 : " << H_2_1 << std::endl;
   std::cout << "Brockett factor : " << tw.exp()  * H_1_0 << std::endl;
   std::cout << "pos H_2_0 " << H_2_0.getTranslation().transpose() << std::endl;
+  std::cout << "eTw " << tw.exp() << std::endl;
   
   return 0;
 }
