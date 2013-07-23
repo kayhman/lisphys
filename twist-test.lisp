32f438c3a8c62153d08caee124f09df64ac67fe0
Guillaume <guillaume.saupin@gmail.com>
2013-07-23 21:55:28 +0200

    Test in progress.

--- twist-test.lisp
+++ twist-test.lisp
@@ -7,10 +7,10 @@
 	(axis #v(0 0 1))
 	(h1_0 (make-instance 'displacement
 			     :pos pos
-			     :rot (make_instance 'quaternion)))
+			     :rot (make-instance 'quaternion)))
 	(h2_1 (make-instance 'displacement
 			     :pos pos
-			     :rot (make_instance 'quaternion)))
+			     :rot (make-instance 'quaternion)))
 	(tw (make-instance 'twist 
 			   :ang axis
 			   :lin (cross pos axis)))) 
@@ -24,24 +24,44 @@
   (let* ((pos #v(1 0 0))
 	(axis #v(0 0 1))
 	(H_1_0 (make-instance 'displacement
-			     :pos pos
+			     :pos #v(1 0 0)
 			     :rot #q(0 0 0 1)))
 	(H_2_1 (make-instance 'displacement
 			     :pos pos
 			     :rot #q(0 0 0 1)))
 	(tw (make-instance 'twist 
-			   :ang axis
-			   :lin (cross pos axis)))
-	 (a (* 1.0 (/ pi 2.0)))) 
+		 :ang axis
+		 :lin (cross pos axis)))
+	 (a (* 2.0 (/ pi 2.0)))
+  ) 
     (progn 
-      (print tw)
-      ;      (print (.* (rot H_1_0) (pos H_2_1)))
-      ;      (print (.* H_1_0 H_2_1) )
-      ;      (print (.exp tw 1e-6))
-      ;      (print (.* (.exp tw 1e-6) H_2_1) )
-      ;      (print (.* (.exp (.* tw a) 1e-6) H_1_0))
-      (print a)
-            (print (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1))
+      (print (list "H_1_0 " H_1_0))
+      (print (list "tw " (.exp tw 1e-6)))
+      (print (.* (.exp tw 1e-6) H_1_0))
+      (print (list "H_2_0" (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1)))
+      
+      )))
+
+
+(defun test-twist-ad ()
+  (let* ((pos #v(1 0 0 ad))
+	(axis #v(0 0 1 ad))
+	(H_1_0 (make-instance 'displacement
+			     :pos pos
+			     :rot #q(0 0 0 1 ad)))
+	(H_2_1 (make-instance 'displacement
+			     :pos pos
+			     :rot #q(0 0 0 1 ad)))
+	(tw (make-instance 'twist 
+		 :ang axis
+		 :lin (cross pos axis)))
+	 (a (* 1.0 (/ pi 2.0)))
+  ) 
+    (progn 
+      (print (list "H_1_0 " H_1_0))
+      (print (list "tw " (.exp tw 1e-6)))
+      (print (.* (.exp tw 1e-6) H_1_0))
+      (print (list "H_2_0" (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1)))
       
       )))
 
