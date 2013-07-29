dc7889bc5cbe72c8f1a864f5c4381d1345314db1
Guillaume <guillaume.saupin@gmail.com>
2013-07-29 10:57:22 +0200

    Do test.

--- twist-test.lisp
+++ twist-test.lisp
@@ -4,65 +4,38 @@
 
 (deftest test-twist (TwistadSuite)
   (let* ((pos #v(1 0 0))
-	(axis #v(0 0 1))
-	(h1_0 (make-instance 'displacement
-			     :pos pos
-			     :rot (make-instance 'quaternion)))
-	(h2_1 (make-instance 'displacement
-			     :pos pos
-			     :rot (make-instance 'quaternion)))
-	(tw (make-instance 'twist 
-			   :ang axis
-			   :lin (cross pos axis)))) 
-    (progn 
-      (print (cross pos axis))
-      ; (assert-equal '(1.0 0.0) (quat-x v1))
-      )))
-
-
-(defun test-twist ()
-  (let* ((pos #v(1 0 0))
-	(axis #v(0 0 1))
-	(H_1_0 (make-instance 'displacement
-			     :pos #v(1 0 0)
-			     :rot #q(0 0 0 1)))
-	(H_2_1 (make-instance 'displacement
-			     :pos pos
-			     :rot #q(0 0 0 1)))
-	(tw (make-instance 'twist 
-		 :ang axis
-		 :lin (cross pos axis)))
+	 (axis #v(0 0 1))
+	 (H_1_0 (make-instance 'displacement
+			       :pos #v(1 0 0)
+			       :rot #q(0 0 0 1)))
+	 (H_2_1 (make-instance 'displacement
+			       :pos pos
+			       :rot #q(0 0 0 1)))
+	 (tw (make-instance 'twist 
+			    :ang axis
+			    :lin (cross pos axis)))
 	 (a (* 2.0 (/ pi 2.0)))
-  ) 
-    (progn 
-      (print (list "H_1_0 " H_1_0))
-      (print (list "tw " (.exp tw 1e-6)))
-      (print (.* (.exp tw 1e-6) H_1_0))
-      (print (list "H_2_0" (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1)))
-      
-      )))
+	 (H_2_0 (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1))
+	 ) 
+    (assert-true
+	(< (norm (pos H_2_0)) 1e-6))))
 
-
-(defun test-twist-ad ()
+(deftest test-twist-ad (TwistadSuite)
   (let* ((pos #v(1 0 0 ad))
-	(axis #v(0 0 1 ad))
-	(H_1_0 (make-instance 'displacement
-			     :pos pos
-			     :rot #q(0 0 0 1 ad)))
-	(H_2_1 (make-instance 'displacement
-			     :pos pos
-			     :rot #q(0 0 0 1 ad)))
-	(tw (make-instance 'twist 
-		 :ang axis
-		 :lin (cross pos axis)))
+	 (axis #v(0 0 1 ad))
+	 (H_1_0 (make-instance 'displacement
+			       :pos #v(1 0 0 ad)
+			       :rot #q(0 0 0 1 ad)))
+	 (H_2_1 (make-instance 'displacement
+			       :pos pos
+			       :rot #q(0 0 0 1 ad)))
+	 (tw (make-instance 'twist 
+			    :ang axis
+			    :lin (cross pos axis)))
 	 (a (* 2.0 (/ pi 2.0)))
-  ) 
-    (progn 
-      (print (list "H_1_0 " H_1_0))
-      (print (list "tw " (.exp tw 1e-6)))
-      (print (.* (.exp tw 1e-6) H_1_0))
-      (print (list "H_2_0" (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1)))
-      
-      )))
+	 (H_2_0 (.* (.* (.exp (.* tw a) 1e-6) H_1_0) H_2_1))
+	 ) 
+    (assert-true
+	(< (val (norm (pos H_2_0))) 1e-6))))
 
 (run-suite 'TwistadSuite)
