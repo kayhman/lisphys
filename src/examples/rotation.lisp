9b2ece6aebceb4f5982b61575ffd53f285daa00a
Guillaume <guillaume.saupin@gmail.com>
2013-08-05 11:55:51 +0200

    More complex rotation example.

--- src/examples/rotation.lisp
+++ src/examples/rotation.lisp
@@ -1,17 +1,23 @@
 (in-package #:lisphys)
 
-(defun rot (q1)
-  (from-axis #v (0 0 1 ad) q1)
-) 
+(setq axis (normalize #v ((random 100) (random 100) (random 100)
+		ad)))
 
-(setq vel #q (0 0 1 0)) ;;pure quaternion
+(defun rotate (q1)
+  (from-axis axis q1))
 
-(setq q1_0 (/ pi 4.0))
 
-(setq drot# (.* (.* (rot q1_0) vel) 0.5)) ;; dot(q) = q * v
+(setq vel #q ((vector3-x axis)	 ;;pure quaternion
+	      (vector3-y axis) 
+	      (vector3-z axis)
+	      0))
 
-(setq drot (der (d-var q1 rot ((q1 q1_0))))) ;; compute dot(q) using add
+(setq q1_0 (* (/ (random 180) 180) pi ))
 
-(d-var q1 rot ((q1 0.7853981)))
+(setq drot# (.* (.* (rotate q1_0) vel) 0.5)) ;; dot(q) = q * v
 
-(.* (.* (conj (rot q1_0)) drot) 2.0);; check that q-1 * dot(q) = vrl
+(setq drot (der (d-var q1 rotate ((q1 q1_0))))) ;; compute dot(q) using add
+
+(d-var q1 rotate ((q1 0.7853981)))
+
+(.* (.* (conj (rotate q1_0)) drot) 2.0);; check that q-1 * dot(q) = vrl
