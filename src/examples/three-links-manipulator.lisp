e56896dc43bb32962ed570a9a7677c2076469a71
Guillaume <guillaume.saupin@gmail.com>
2013-09-10 23:20:34 +0200

    First integrator.
    * Todo : more simple example : pendulum.

--- src/examples/three-links-manipulator.lisp
+++ src/examples/three-links-manipulator.lisp
@@ -40,24 +40,52 @@
 
 (defvar **gravity** #m((0) (0) (1) (0) (0) (0)) )
 
-(setq m1 (ball-inertia 1.0 0.3))
+(setf m1 (ball-inertia 1.0 0.3))
 (setq m2 (ball-inertia 1.0 0.3))
 (setq m3 (ball-inertia 1.0 0.3))
 
-(setq Jt1 (jacobian-transpose g_l1_s ((q1 0) (q2 0) (q3 0))))
-(setq Jt2 (jacobian-transpose g_l2_s ((q1 0) (q2 0) (q3 0))))
-(setq Jt3 (jacobian-transpose g_l3_s ((q1 0) (q2 0) (q3 0))))
+(setq x1 0.0)
+(setq x2 0.0)
+(setq x3 0.0)
 
-(setq mg1 (.* (val Jt1) (.* m1 (val (transpose Jt1)))))
-(setq mg2 (.* (val Jt2) (.* m1 (val (transpose Jt2)))))
-(setq mg3 (.* (val Jt3) (.* m1 (val (transpose Jt3)))))
+(let ((x1 0.)
+      (x2 0.)
+      (x3 0.)
+      (v1 0.)
+      (v2 0.)
+      (v3 0.)
+      Jt1 Jt2 Jt3 mg1 mg2 mg3 mg rhs)
+  (defun integrate (dt)
+    (progn
+      (setf Jt1 (jacobian-transpose g_l1_s ((q1 x1) (q2 x2) (q3 x3))))
+      (setf Jt2 (jacobian-transpose g_l2_s ((q1 x1) (q2 x2) (q3 x3))))
+      (setf Jt3 (jacobian-transpose g_l3_s ((q1 x1) (q2 x2) (q3 x3))))
+    
+      (setf mg1 (.* (val Jt1) (.* m1 (val (transpose Jt1)))))
+      (setf mg2 (.* (val Jt2) (.* m1 (val (transpose Jt2)))))
+      (setf mg3 (.* (val Jt3) (.* m1 (val (transpose Jt3)))))
+    
+      (setf mg (.+ (.+ mg1 mg2) mg3))
+      (setf rhs (.+ (.+ (.* (val Jt1) **gravity**) 
+			(.* (val Jt2) **gravity**))
+		    (.* (val Jt3) **gravity**)))
+      (multiple-value-bind  (L U P) (lu (val mg))
+	(let ((x #m((0) (0) (0))))
+	  (progn
+	    (solve P L U x rhs)
+	    (setf v1 (+ v1 (* dt (mref x 0 0)))) 
+	    (setf v2 (+ v2 (* dt (mref x 1 0))))
+	    (setf v3 (+ v3 (* dt (mref x 2 0))))
+	    (setf x1 (+ x1 (* dt v1))) 
+	    (setf x2 (+ x2 (* dt v2))) 
+	    (setf x3 (+ x3 (* dt v3)))
+	    )))
+      (with-open-file (stream "traj.txt")
+	(format stream "~a ~a ~a ~a ~%" (read-line stream)))
+      (print (list x1 x2 x3)))))
 
-(setq mg (.+ (.+ mg1 mg2) mg3))
 
+(loop for i from 0 to 10000 do (integrate 1e-3))
 
 ;; Time Integration 
-(multiple-value-bind  (L U P) (lu (val mg))
-  (let ((x #v(0 0 0)))
-    (progn
-      (solve P L U x **gravity**)
-      (print x))))
+
