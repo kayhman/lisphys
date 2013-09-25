49f6040afbe6415c6e11cdbd276428ae4fcc90e5
Guillaume <guillaume.saupin@gmail.com>
2013-09-25 23:54:03 +0200

    Clean code.
    * Todo : more progn to remove.

--- src/lib/jacobian.lisp
+++ src/lib/jacobian.lisp
@@ -8,5 +8,5 @@
 
 (defmacro jacobian-transpose (f bindings)
   `(let ((jacobian (make-instance 'matrix :nrows (length ',bindings) :ncols 6)))
-     (progn ,@(loop for n in bindings collect `(setf (row jacobian (position ',n ',bindings)) (jacobian-col ,f ,(first n) ,@bindings))))
+     ,@(loop for n in bindings collect `(setf (row jacobian (position ',n ',bindings)) (jacobian-col ,f ,(first n) ,@bindings)))
      jacobian))
